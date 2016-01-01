f888d5f4f99cbec5c43dfbda8f172d4569a97639
Philipp Bock <p@philippbock.de>
2016-01-01 14:27:24 +0100

    Use the tileserver that's accessible from the captive portal

--- src/assets/js/index.jsx
+++ src/assets/js/index.jsx
@@ -79,7 +79,7 @@ let App = React.createClass({
       <Speedometer speed={this.state.speed} fixes={this.state.fixes} />
       <Map center={ this.state.position } zoom={this.state.zoom || 12} className="map" onZoomend={ev => this.setState({ zoom: ev.target._animateToZoom })}>
         <TileLayer
-          url="http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png"
+          url="http://ice.portal/maps/{z}/{x}/{y}.png"
           attribution={'&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}
         />
         <Polyline
